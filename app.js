43ab452509f73ea5098507c0fa993625b7ad0d93
EngStu <engstu2015@gmail.com>
2018-02-22 10:09:22 +0630

    updated node version

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use(helmet());
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb://cooluser:coolpassword@ds119748.mlab.com:19748/local_library'
+// var dev_db_url = 'mongodb://cooluser:coolpassword@ds119748.mlab.com:19748/local_library'
+var dev_db_url = 'mongodb://engstu:engstu@ds143388.mlab.com:43388/local_library'
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
